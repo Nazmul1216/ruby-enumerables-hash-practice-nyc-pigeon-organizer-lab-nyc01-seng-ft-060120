0f205e2bec6f3392983153ed3b0d08afe2c7b984
Nazmul Kabir <nazmul.kabir1216@gmail.com>
2020-05-20 02:28:21 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -4,9 +4,8 @@ def nyc_pigeon_organizer(data)
   data.each do|k, v|
     v.each do|inner_key, names|
       names.each do |n|
-        temp_hash = { :color=>[], :gender=>[], :lives=>[] }
         if !result.has_key?(n)
-          result[n]=temp_hash
+          result[n] = { :color=>[], :gender=>[], :lives=>[] }
         end
       end
     end 
