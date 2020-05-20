9b4e07df69939b876161cd42c15706f13e693938
Nazmul Kabir <nazmul.kabir1216@gmail.com>
2020-05-20 02:29:06 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,11 +1,12 @@
 
 def nyc_pigeon_organizer(data)
-  result={}
+  result = {}
   data.each do|k, v|
     v.each do|inner_key, names|
       names.each do |n|
         if !result.has_key?(n)
-          result[n] = { :color=>[], :gender=>[], :lives=>[] }
+          p n
+          result[n] = {:color=>[], :gender=>[], :lives=>[]}
         end
       end
     end 
